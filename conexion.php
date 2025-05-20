38da885f33eac8daa93eff9685fd9b8d2804b5ce
Aldo.G <130008534+Aldo14G@users.noreply.github.com>
2025-05-20 15:40:38 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,12 +1,9 @@
 <?php
-// Archivo de conexión a la base de datos del gimnasio
 $conexion = mysqli_connect("localhost", "root", "", "galaxy_gym");
 
-// Verificar conexión
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
 
-// Configurar el juego de caracteres de la conexión
 mysqli_set_charset($conexion, "utf8");
 ?>
